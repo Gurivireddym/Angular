98be420d79411066764d4d45ce6114ca2adeb9bc
Gurivireddym <97427817+Gurivireddym@users.noreply.github.com>
2023-03-14 15:39:05 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BmiComponent } from './bmi/bmi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BmiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
